27f93ecccdd86d67ba95df3860c2094abda19bd5
wensug <wensug@gmail.com>
2019-05-16 17:30:20 +0100

    footer

--- src/frontend/components/footer/Footer.js
+++ src/frontend/components/footer/Footer.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Footer.css'
 
 export default function Footer() {
     return (
-        <div className='Footer'>
-            <div className='social-media'>
-                <a href='#'><i class="fab fa-facebook"></i></a>
-                <a href='#'></a>
-                <a href='#'></a>
-                <a href='#'></a>
+        <footer>
+        <div className='footer-main'>
+            <ul className='footer-navbar'>
+                <li><NavLink to='/#home'>Product</NavLink></li>
+                <li><NavLink to='/#About'>About</NavLink></li>
+                <li><NavLink to='/#LogIn'>LogIn</NavLink></li>
+                <li><NavLink to='/#Contact'>Contact</NavLink></li>
+            </ul>
+            <div className='footer-media'>
+                <a><i class="fab fa-facebook"></i></a>
+                <a><i class="fab fa-instagram"></i></a>
+                <a><i class="fab fa-twitter"></i></a>
+                <a><i class="fab fa-youtube"></i></a>
             </div>
-            <p>© 2019 CarrU | All Rights Reserved</p>
         </div>
+        <div className='footer-text'><p>© 2019 CarrU | All Rights Reserved</p></div>
+    </footer>
     );
 }
