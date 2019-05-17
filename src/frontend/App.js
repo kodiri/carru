1ee57faff3f073605400c4eb2f1cdd2a1a278a00
wensug <wensug@gmail.com>
2019-05-17 11:00:39 +0100

    New Group Form

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,10 +2,8 @@ import React, {Component} from 'react';
 import { HashRouter as Router} from 'react-router-dom';
 import Header from './components/header/Header';
 import Homepage from './components/homepage/Homepage';
-import Footer from './components/footer/Footer';
 
 import './App.css';
-
 
 class App extends Component {
   render() {
@@ -14,7 +12,6 @@ class App extends Component {
         <div className='App'>
           <Header />
           <Homepage />
-          <Footer />
         </div>
       </Router>
     );
