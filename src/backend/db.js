e2ec8fca289a5c87837bbb390cb835c4d1d8269b
wensug <wensug@gmail.com>
2019-05-30 17:32:59 +0100

    fixed user form

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,13 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 require('dotenv').config();
-const url = process.env.NODE_ENV === process.env.PRODUCTION ?
+const url = process.env.NODE_ENV === 'production' ?
     process.env.DB_URL_PRD :
     process.env.DB_URL_DEV;
    const dbName = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('?'));
 
-   module.exports = { connect };
-console.log(url)
+module.exports = { connect };
 function connect() {
     return new Promise((resolve, reject) => {
          MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
